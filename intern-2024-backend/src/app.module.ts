4c43079521b80c8e880541dfb5c855ee58c28ddc
harsh-maximl <harsh.gupta@maximl.com>
2024-02-14 22:27:55 +0530

    database connection

--- intern-2024-backend/src/app.module.ts
+++ intern-2024-backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(
+    {
+      type: 'postgres',
+      host: "ep-gentle-king-a2bt7a21-pooler.eu-central-1.aws.neon.tech",
+      port: 5432,
+      username: "default",
+      password: "KslXvpunw89D",
+      database: "verceldb",
+      entities: [],
+      synchronize: true,
+      ssl: true
+    }
+  ), TypeOrmModule.forFeature()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
