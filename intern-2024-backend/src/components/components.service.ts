884662aff2d3217889fcbc23e527e5adc1e982c8
harsh-maximl <harsh.gupta@maximl.com>
2024-02-22 10:28:50 +0530

    components functionality added

--- intern-2024-backend/src/components/components.service.ts
+++ intern-2024-backend/src/components/components.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { ComponentEntity } from './entities/components.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class ComponentsService {
@@ -10,6 +11,44 @@ export class ComponentsService {
   ) { }
 
   async getAllComponents() {
+    const components = await this.components.find();
+    if (!components.length) throw new NotFoundException("Components not found");
+    return components;
+  }
 
+  async getComponent(id: string) {
+    const component = await this.components.findOne({ where: { id } });
+    if (!component) throw new NotFoundException("No Component Found");
+    return component;
+  }
+
+  async createComponent(content) {
+    const name = content.name;
+    const type = content.component;
+    delete content.id
+    const component = await this.components.create({ name, component: type });
+    Object.assign(component, content['e72ad8d8-062c-43bd-bd49-80a8a969de56']);
+    // console.log(component);
+    const componentSave = await this.components.save(component);
+    // console.log(componentSave);
+
+    return componentSave
+  }
+
+  async updateComponent(id: string, attr: Partial<ComponentEntity>) {
+    const findComponent = await this.components.exists({ where: { id } })
+    if (!findComponent) throw new NotFoundException("No Component Found");
+    const component = await this.components.findOne({ where: { id } });
+    // console.log(attr);
+    Object.assign(component, attr);
+    // console.log(component);
+    return await this.components.save(component);
+  }
+
+  async deleteComponent(id: string) {
+    const findComponent = await this.components.exists({ where: { id } })
+    if (!findComponent) throw new NotFoundException("No Component Found");
+    const deleteComponent = await this.components.delete(id);
+    return "Component Deleted Successfully";
   }
 }
--- intern-2024-backend/src/components/entities/components.entity.ts
+++ intern-2024-backend/src/components/entities/components.entity.ts
@@ -15,39 +15,6 @@ export class ComponentEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
-  name: string;
-
-  @Column()
-  type: string;
-
-  // @Column({ nullable: true })
-  // parent: string;
-
-  @Column('simple-json')
-  properties: any;
-
-  @Column('simple-json', { name: 'general_properties', nullable: true })
-  general: any;
-
-  @Column('simple-json')
-  styles: any;
-
-  @Column('simple-json', { name: 'general_styles', nullable: true })
-  generalStyles: any;
-
-  @Column('simple-json', { name: 'display_preferences', nullable: true })
-  displayPreferences: any;
-
-  @Column('simple-json')
-  validation: any;
-
-  @CreateDateColumn({ default: () => 'now()', name: 'created_at' })
-  createdAt: Date;
-
-  @UpdateDateColumn({ default: () => 'now()', name: 'updated_at' })
-  updatedAt: Date;
-
   @Column({ type: 'double precision' })
   top: number;
 
@@ -59,5 +26,63 @@ export class ComponentEntity {
 
   @Column({ type: 'double precision' })
   height: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  component: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  displayName: string;
+
+  // @Column({ nullable: true })
+  // parent: string;
+
+
+
+  @Column('simple-json')
+  properties: any;
+
+  @Column('simple-json', { name: 'general_properties', nullable: true })
+  general: any;
+
+  @Column('simple-json')
+  others: any;
+
+  @Column('simple-json')
+  events: any;
+
+  @Column('simple-json')
+  styles: any;
+
+  @Column('simple-json')
+  validate: any;
+
+  @Column('simple-json', { name: 'general_styles', nullable: true })
+  generalStyles: any;
+
+  @Column('simple-json')
+  definition: any;
+
+  @Column('simple-json')
+  defaultSize: any;
+
+  @Column('simple-json')
+  exposedVariables: any;
+
+  @Column('simple-json')
+  actions: any;
+
+  @CreateDateColumn({ default: () => 'now()', name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ default: () => 'now()', name: 'updated_at' })
+  updatedAt: Date;
+
+
 
 }
