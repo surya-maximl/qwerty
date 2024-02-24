73b55b68c6d7dc647d77515d80d516ffc826546a
harsh-maximl <harsh.gupta@maximl.com>
2024-02-24 21:32:05 +0530

    fix:user id

--- server/src/component/component.controller.ts
+++ server/src/component/component.controller.ts
@@ -12,33 +12,33 @@ export class ComponentController {
 
   @Get()
   async getAllComponent(@User() user: any) {
-    const components = await this.componentService.getAllComponents(user.id);
+    const components = await this.componentService.getAllComponents(user?.id);
     return components
   }
 
   @Get(':id')
   async getComponent(@Param('id') id: string, @User() user: any) {
-    const component = await this.componentService.getComponent(id, user.id);
+    const component = await this.componentService.getComponent(id, user?.id);
     return component
   }
 
   @Post()
   async createComponent(@Body() content, @User() user: any) {
-    const component = await this.componentService.createComponent(content, user.id);
+    const component = await this.componentService.createComponent(content, user?.id);
     return component;
   }
 
 
   @Patch(':id')
   async updateComponent(@Param('id') id: string, @Body() attr: Partial<ComponentEntity>, @User() user: any) {
-    const component = await this.componentService.updateComponent(id, attr, user.id);
+    const component = await this.componentService.updateComponent(id, attr, user?.id);
     return component;
   }
 
   @Delete(':id')
   async deleteComponent(@Param('id') id: string, @User() user: any) {
     // return id;
-    const component = await this.componentService.deleteComponent(id, user.id);
+    const component = await this.componentService.deleteComponent(id, user?.id);
     return component;
   }
 
