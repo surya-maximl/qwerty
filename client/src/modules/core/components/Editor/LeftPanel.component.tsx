1fd469e70c6719225eefa70bb4b9f4e0426055ec
utk9rsh8 <utkarsh.singh@maximl.com>
2024-02-24 19:17:12 +0530

    feat: Added dashboard header and profile settings

--- client/src/modules/core/components/Editor/LeftPanel.component.tsx
+++ client/src/modules/core/components/Editor/LeftPanel.component.tsx
@@ -1,8 +1,5 @@
 import { SyntheticEvent } from 'react';
-import { Avatar, Dropdown, Flex, Layout, MenuProps } from 'antd';
-
-import { logout } from '../../../authentication/reducers';
-import { useAppDispatch } from '../../../shared/hooks/useAppDispatch';
+import { Avatar, Dropdown, Flex, Layout } from 'antd';
 import { useAuth } from '../../../shared/hooks/useAuth';
 
 const { Sider } = Layout;
@@ -14,32 +11,6 @@ const leftPanelStyle: React.CSSProperties = {
 
 const LeftPanel: React.FC = () => {
   const { user } = useAuth();
-  const dispatch = useAppDispatch();
-
-  const items: MenuProps['items'] = [
-    {
-      key: 'profile-menu-name',
-      label: (
-        <span className="flex flex-col">
-          <span className="font-semibold">{user.username}</span>
-          {user.email}
-        </span>
-      )
-    },
-    {
-      type: 'divider'
-    },
-    {
-      label: <span className="text-red-400">Log Out</span>,
-      key: '2'
-    }
-  ];
-
-  const onClick: MenuProps['onClick'] = ({ key }) => {
-    if (key === '2') {
-      dispatch(logout());
-    }
-  };
 
   return (
     <Sider collapsed collapsedWidth={48} style={leftPanelStyle}>
@@ -50,23 +21,6 @@ const LeftPanel: React.FC = () => {
             src="https://app.tooljet.com/logo.svg"
             alt="Tooljet Logo"
           />
-        </Flex>
-        <Flex align="center" justify="flex-end" className="flex-col p-1 py-4 flex-1">
-          <Dropdown menu={{ items, onClick }} trigger={['click']}>
-            <div
-              onClick={(event: SyntheticEvent) => {
-                event.preventDefault();
-              }}
-              className="flex items-center cursor-pointer"
-            >
-              <Avatar
-                className="hover:scale-110 transition ease-in-out delay-100"
-                style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}
-              >
-                {user.username?.substring(0, 1)}
-              </Avatar>
-            </div>
-          </Dropdown>
         </Flex>
       </Flex>
     </Sider>
--- client/src/modules/core/pages/Dashboard/dashboard.tsx
+++ client/src/modules/core/pages/Dashboard/dashboard.tsx
@@ -3,17 +3,17 @@ import { Button, Dropdown, Flex, Image, Input, Layout, Typography } from 'antd';
 import axios from 'axios';
 import Fuse from 'fuse.js';
 import { IoMdAdd } from 'react-icons/io';
+import { MdDelete, MdDriveFileRenameOutline, MdOutlineChangeCircle } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+
 import dashboardImage from '../../../../assets/dashboard.svg';
 import AppCard from '../../components/AppCard/AppCard.component';
+import DashboardHeader from '../../components/Dashboard/DashboardHeader.component';
 import LeftPanel from '../../components/Editor/LeftPanel.component';
 import Modal from '../../components/Modal/Modal.component';
 import { appType } from '../../interfaces/dashboard.interface';
 import { getCookie } from '../../utils/authUtils';
-import { MdOutlineChangeCircle } from "react-icons/md";
-import { MdDriveFileRenameOutline } from "react-icons/md";
-import { MdDelete } from "react-icons/md";
-import { useNavigate } from 'react-router-dom';
-
+import UserInfoModal from '../../components/Modal/UserInfoModal.component';
 
 const Dashboard = () => {
   const [open, setOpen] = useState(false);
@@ -25,6 +25,8 @@ const Dashboard = () => {
   const [token, setToken] = useState('');
   const [filteredApps, setFilteredApps] = useState<appType[]>([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [userInfoOpen, setUserInfoOpen] = useState(false);
+
   const navigate = useNavigate();
 
   const handleCreateApp = async () => {
@@ -69,7 +71,9 @@ const Dashboard = () => {
   }, []);
 
   useEffect(() => {
-    fetchAllApps();
+    if (token !== '') {
+      fetchAllApps();
+    }
   }, [refresh, token]);
 
   const renameApp = async (id: string) => {
@@ -126,10 +130,10 @@ const Dashboard = () => {
   };
 
   const handleAppEvents = (method: string, id: string) => {
-    console.log("handleappeventd")
+    console.log('handleappeventd');
     setMethod(method);
     setClickedId(id);
-    setSearchQuery("");
+    setSearchQuery('');
     setOpen(true);
   };
   const handleDropdownClick = (e: any, id: string) => {
@@ -160,38 +164,55 @@ const Dashboard = () => {
 
   const { Content } = Layout;
 
-
-  let items = filteredApps.length>0 ? filteredApps.map((item) => {
-    return {
-      key: item.id,
-      label: (
-        <Flex align='center' className='cursor-default' >
-          <Flex flex={10} onClick={() => navigate(`/app/editor/${item.id}`)}>
-          <Typography className='text-md font-semibold text-primary'>{item?.name}</Typography>
-          </Flex>
-          <Flex gap={10} flex={1}>
-          <Typography className='p-0 text-xl font-semibold text-primary cursor-pointer hover:text-black' onClick={() => handleAppEvents('renameApp', item.id)}><MdDriveFileRenameOutline/></Typography>
-          <Typography className='p-0 text-xl font-semibold text-primary cursor-pointer hover:text-black' onClick={() => handleAppEvents('changeIcon', item.id)}><MdOutlineChangeCircle/></Typography>
-          <Typography className='p-0 text-xl font-semibold text-[#ff7875] cursor-pointer hover:text-[#f5222d]' onClick={() => deleteApp(item.id)}><MdDelete/></Typography>
-          </Flex>
-        </Flex>
-      )
-    };
-  }) : [
-    {
-      key: '1',
-      label: (
-        <Typography className='text-md text-primary'>
-          No apps match your search!
-        </Typography>
-      ),
-      disabled: true
-    }
-  ];
+  let items =
+    filteredApps.length > 0
+      ? filteredApps.map((item) => {
+          return {
+            key: item.id,
+            label: (
+              <Flex align="center" className="cursor-default">
+                <Flex flex={10} onClick={() => navigate(`/app/editor/${item.id}`)}>
+                  <Typography className="text-md font-semibold text-primary">
+                    {item?.name}
+                  </Typography>
+                </Flex>
+                <Flex gap={10} flex={1}>
+                  <Typography
+                    className="p-0 text-xl font-semibold text-primary cursor-pointer hover:text-black"
+                    onClick={() => handleAppEvents('renameApp', item.id)}
+                  >
+                    <MdDriveFileRenameOutline />
+                  </Typography>
+                  <Typography
+                    className="p-0 text-xl font-semibold text-primary cursor-pointer hover:text-black"
+                    onClick={() => handleAppEvents('changeIcon', item.id)}
+                  >
+                    <MdOutlineChangeCircle />
+                  </Typography>
+                  <Typography
+                    className="p-0 text-xl font-semibold text-[#ff7875] cursor-pointer hover:text-[#f5222d]"
+                    onClick={() => deleteApp(item.id)}
+                  >
+                    <MdDelete />
+                  </Typography>
+                </Flex>
+              </Flex>
+            )
+          };
+        })
+      : [
+          {
+            key: '1',
+            label: (
+              <Typography className="text-md text-primary">No apps match your search!</Typography>
+            ),
+            disabled: true
+          }
+        ];
 
   return (
     <Layout className="min-h-screen">
-      <LeftPanel />
+      <UserInfoModal setOpen={setUserInfoOpen} open={userInfoOpen}/>
       <Modal
         renameApp={renameApp}
         handleCreateApp={handleCreateApp}
@@ -203,62 +224,68 @@ const Dashboard = () => {
         id={clickedId}
         method={method}
       />
-      <Content>
-        <Flex className="h-full " vertical flex={4}>
-          <Flex flex={1} justify="center" className="p-10">
-            <Button
-              type="primary"
-              size="large"
-              className="mr-4 flex items-center"
-              onClick={() => {
-                setMethod('createApp');
-                setOpen(true);
-              }}
-            >
-              <IoMdAdd className="text-xl mr-1" />
-              Create App
-            </Button>
-            <Dropdown menu={{ items }} open={searchQuery !== ''}>
-              <Input.Search
-                placeholder="search apps"
-                allowClear
-                className="h-fit"
-                style={{ width: '50%' }}
-                size="large"
-                value={searchQuery}
-                onChange={(e) => handleSearchQueryChange(e)}
-              />
-            </Dropdown>
-          </Flex>
-          <Flex className="gap-[4%] px-[4%]" flex={5} wrap="wrap">
-            {apps.length === 0 && (
-              <Flex justify="center" className="w-full h-[80%]">
-                <Flex align="flex-end" justify="center" vertical className="w-full" flex={1}>
-                  <h1 className="text-primary text-4xl">Welcome to your new Workspace!</h1>
-                  <p className="text-xl">You can get started by creating a new application</p>
-                  <Button className="mt-10 flex items-center" type="primary" size="large">
-                    <IoMdAdd className="text-xl mr-1" />
-                    Create New App
-                  </Button>
-                </Flex>
-                <Flex flex={1} justify="center" align="center" className="hidden lg:block">
-                  <Image
-                    width={500}
-                    src={dashboardImage}
-                    preview={false}
-                    alt="Create app image"
-                    className="mt-6"
+      <Layout>
+        <LeftPanel />
+        <Layout>
+        <DashboardHeader setOpen={setUserInfoOpen} open={userInfoOpen}/>
+          <Content>
+            <Flex className="h-full " vertical flex={4}>
+              <Flex flex={1} justify="center" className="p-10">
+                <Button
+                  type="primary"
+                  size="large"
+                  className="mr-4 flex items-center"
+                  onClick={() => {
+                    setMethod('createApp');
+                    setOpen(true);
+                  }}
+                >
+                  <IoMdAdd className="text-xl mr-1" />
+                  Create App
+                </Button>
+                <Dropdown menu={{ items }} open={searchQuery !== ''}>
+                  <Input.Search
+                    placeholder="search apps"
+                    allowClear
+                    className="h-fit"
+                    style={{ width: '50%' }}
+                    size="large"
+                    value={searchQuery}
+                    onChange={(e) => handleSearchQueryChange(e)}
                   />
-                </Flex>
+                </Dropdown>
               </Flex>
-            )}
-            {apps.length !== 0 &&
-              apps.map((item) => {
-                return <AppCard item={item} handleDropdownClick={handleDropdownClick} />;
-              })}
-          </Flex>
-        </Flex>
-      </Content>
+              <Flex className="gap-[4%] px-[4%]" flex={5} wrap="wrap">
+                {apps.length === 0 && (
+                  <Flex justify="center" className="w-full h-[80%]">
+                    <Flex align="flex-end" justify="center" vertical className="w-full" flex={1}>
+                      <h1 className="text-primary text-4xl">Welcome to your new Workspace!</h1>
+                      <p className="text-xl">You can get started by creating a new application</p>
+                      <Button className="mt-10 flex items-center" type="primary" size="large">
+                        <IoMdAdd className="text-xl mr-1" />
+                        Create New App
+                      </Button>
+                    </Flex>
+                    <Flex flex={1} justify="center" align="center" className="hidden lg:block">
+                      <Image
+                        width={500}
+                        src={dashboardImage}
+                        preview={false}
+                        alt="Create app image"
+                        className="mt-6"
+                      />
+                    </Flex>
+                  </Flex>
+                )}
+                {apps.length !== 0 &&
+                  apps.map((item) => {
+                    return <AppCard item={item} handleDropdownClick={handleDropdownClick} />;
+                  })}
+              </Flex>
+            </Flex>
+          </Content>
+        </Layout>
+      </Layout>
     </Layout>
   );
 };
