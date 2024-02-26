d58653c0969a22b2ac2631a71a2e61b8a150448e
harsh-maximl <harsh.gupta@maximl.com>
2024-02-26 08:59:50 +0530

    fix: added production and development settings

--- server/src/config/database.module.ts
+++ server/src/config/database.module.ts
@@ -1,20 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+require('dotenv').config()
+
 
 @Module({
     imports: [
         TypeOrmModule.forRootAsync({
             useFactory: async (configService: ConfigService) => ({
                 type: 'postgres',
-                host: "dpg-cnborled3nmc73ai33l0-a.oregon-postgres.render.com",
-                port: 5432,
-                username: "tooljet_nusq_user",
-                password: "ISIrcjg6p1HbBB5AgZjYnP4VpKjl84DO",
-                database: "tooljet_nusq",
-                autoLoadEntities: true,
-                synchronize: true,
-                ssl: true,
+                url: process.env.NODE_ENV = 'dev' ? process.env.DEV_DATABASE_URL : process.env.PROD_DATABASE_URL
             }),
             inject: [ConfigService]
         })
