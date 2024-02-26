b8f401bf7a94ebf9a2986d1bd93e23a2e88f66ae
harsh-maximl <harsh.gupta@maximl.com>
2024-02-26 10:07:38 +0530

    fix: db error synchronize

--- server/src/config/database.module.ts
+++ server/src/config/database.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { User } from 'src/user/auth/entities/user.entity';
+import { AppEntity } from 'src/apps/entities/apps.entity';
+import { ComponentEntity } from 'src/component/entities/component.entity';
 require('dotenv').config()
 
 
@@ -9,7 +12,9 @@ require('dotenv').config()
         TypeOrmModule.forRootAsync({
             useFactory: async (configService: ConfigService) => ({
                 type: 'postgres',
-                url: process.env.NODE_ENV = 'dev' ? process.env.DEV_DATABASE_URL : process.env.PROD_DATABASE_URL
+                url: process.env.NODE_ENV = 'dev' ? process.env.DEV_DATABASE_URL : process.env.PROD_DATABASE_URL,
+                synchronize: true,
+                entities: [User, AppEntity, ComponentEntity]
             }),
             inject: [ConfigService]
         })
